35a9780db110ac299bdae4f9111e3c97667fe1e1
Zach Smith <Lemelisk27@gmail.com>
2021-12-30 21:47:51 -0800

    added more app routes

--- controllers/api/app-routes.js
+++ controllers/api/app-routes.js
@@ -46,4 +46,71 @@ router.post("/", tokenAuth, (req, res) => {
     })
 })
 
+router.get("/:id", tokenAuth, (req, res) => {
+    App.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: {
+            include: [[sequelize.fn('date_format', sequelize.col('applied'), '%m-%d-%Y'), 'applied_date'],[sequelize.fn('date_format', sequelize.col('closed'), '%m-%d-%Y'), 'closed_date']]
+        }
+    })
+    .then(foundApp=>{
+        if (!foundApp) {
+            res.status(404).json({Message: "Nothing Found"})
+        }
+        else {
+            res.json(foundApp)
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.put("/close", tokenAuth, (req, res) => {
+    App.update({
+        closed: req.body.closed,
+        open: req.body.open
+    },
+    {
+        where: {
+            id: req.body.id
+        }
+    })
+    .then(updatedApp=>{
+        res.json(updatedApp)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.put("/", tokenAuth, (req, res) => {
+    App.update({
+        UserId: req.body.UserId,
+        applied: req.body.applied,
+        closed: req.body.closed,
+        employer: req.body.employer,
+        link: req.body.link,
+        notes: req.body.notes,
+        open: req.body.open,
+        title: req.body.title,
+    },
+    {
+        where: {
+            id: req.body.id
+        }
+    })
+    .then(updatedApp=>{
+        res.json(updatedApp)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
 module.exports = router
